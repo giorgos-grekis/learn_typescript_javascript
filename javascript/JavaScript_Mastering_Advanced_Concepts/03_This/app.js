4f2ed3c92983247eff7a8c2936eac466f5647c35
giorgos-grekis <georgrekis.github@gmail.com>
2024-11-09 14:48:18 +0200

    The "Left Of The Dot" Rule

--- javascript/JavaScript_Mastering_Advanced_Concepts/03_This/app.js
+++ javascript/JavaScript_Mastering_Advanced_Concepts/03_This/app.js
@@ -1,20 +1,21 @@
-// const person = {
-//   name: "Conan",
-//   city: "Los Angeles",
-//   sing() {
-//     return `${this.name} sings LA LA LA`;
-//   },
-// };
+const person = {
+  name: "Conan",
+  city: "Los Angeles",
+  sing() {
+    console.log("THIS is:", this);
+    return `${this.name} sings LA LA LA`;
+  },
+};
 
-// class Cat {
-//   constructor(firstName) {
-//     this.firstName = firstName;
-//   }
+class Cat {
+  constructor(firstName) {
+    this.firstName = firstName;
+  }
 
-//   dance(style = "tango") {
-//     return `Meow I am ${this.firstName} and I like to ${style}`;
-//   }
-// }
+  dance(style = "tango") {
+    return `Meow I am ${this.firstName} and I like to ${style}`;
+  }
+}
 
 function greet() {
   return "HI THERE!!";
@@ -26,3 +27,13 @@ function greet() {
  *  - in browser is typically, "window" Object
  *  - in server (nodejs) is "global" Object
  * */
+
+function whatIsThis() {
+  console.log("The value of this is: ", this);
+}
+
+const obj = {
+  color: "purple",
+  age: 2,
+  whatIsThis: whatIsThis,
+};
