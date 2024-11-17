f492c5830a7851c7dcdac01b96b4ab8f403d3db1
giorgos-grekis <georgrekis.github@gmail.com>
2024-11-17 19:26:03 +0200

    A Simple Compose Function

--- javascript/JavaScript_Mastering_Advanced_Concepts/10_FunctionalProgramming/10.simpleCompose.js
+++ javascript/JavaScript_Mastering_Advanced_Concepts/10_FunctionalProgramming/10.simpleCompose.js
@@ -1,3 +1,5 @@
+// You can think of this function as f(g(x))
+// Take x, pass it to the first function g and then take that value and pass it to f.
 function compose(fn1, fn2) {
   return function (val) {
     return fn1(fn2(val));
