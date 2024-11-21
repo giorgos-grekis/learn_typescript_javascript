28a41608b667d76b2ed447975f84d6334a745b72
giorgos-grekis <georgrekis.github@gmail.com>
2024-11-21 22:30:32 +0200

    complete section 12 web storage

---
+++ javascript/JavaScript_Mastering_Advanced_Concepts/10_FunctionalProgramming/FP-Stylizer/app.js
@@ -0,0 +1,62 @@
+function compose(...functions) {
+  return function (result) {
+    return functions.reduceRight(function (prevResult, fn) {
+      return fn(prevResult);
+    }, result);
+  };
+}
+
+// Your code goes here!
+const applyBtn = document.getElementById("apply-styles");
+const outputField = document.getElementById("styled-text");
+
+const inputs = {
+  content: document.getElementById("input-text"),
+  "text-transform": document.getElementById("uppercase"),
+  color: document.getElementById("color-picker"),
+  "font-size": document.getElementById("font-size"),
+  "font-family": document.getElementById("font-family"),
+};
+
+const outputStyle = outputField.style;
+
+function getVal(source, prop) {
+  return source[prop].value;
+}
+
+const getInputVal = (prop) =>
+  // Custom case for the checkbox input:
+  prop === "text-transform"
+    ? inputs[prop].checked
+      ? "uppercase"
+      : "none"
+    : // Standart approach
+      getVal(inputs, prop);
+
+function setStyle(style, prop) {
+  style[prop] = getInputVal(prop);
+}
+
+const setters = {
+  setCase: () => {
+    setStyle(outputStyle, "text-transform");
+  },
+  setColor: () => {
+    setStyle(outputStyle, "color");
+  },
+  setSize: () => {
+    setStyle(outputStyle, "font-size");
+  },
+  setFont: () => {
+    setStyle(outputStyle, "font-family");
+  },
+};
+
+const setOutputStyle = compose(...Object.values(setters));
+
+applyBtn.addEventListener("click", () => {
+  outputField.innerText = getInputVal("content");
+  setOutputStyle(outputStyle);
+  // console.log('el:', outputField);
+  // console.log('el.style:', outputField.style);
+});
