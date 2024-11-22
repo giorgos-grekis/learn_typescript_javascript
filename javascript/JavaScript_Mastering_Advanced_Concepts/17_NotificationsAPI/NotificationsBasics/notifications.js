22f05dd57a93bd3371a491d03cab6bf3b90d02ee
giorgos-grekis <georgrekis.github@gmail.com>
2024-11-22 13:14:29 +0200

    complete with section 17 notification

---
+++ javascript/JavaScript_Mastering_Advanced_Concepts/17_NotificationsAPI/NotificationsBasics/notifications.js
@@ -0,0 +1,33 @@
+/**
+ * Enables web pages to display notifications to users.
+ */
+
+let notification;
+async function showNotification() {
+  const permission = await Notification.requestPermission();
+
+  console.log("permission: ", permission);
+  if (permission === "granted") {
+    new Notification("Look at my cat!", {
+      body: "This is the body of my first notification! I'm so excited :) ",
+      icon: "blue.png",
+      data: {
+        url: "blah.com",
+        person: "timbo jimbo",
+      },
+    });
+
+    notification.addEventListener("click", () => {
+      console.log("CLICKED THE NOTIFICATION!");
+      console.log(notification);
+      window.focus();
+      notification.close();
+    });
+
+    notification.addEventListener("close", () => {
+      console.log("CLOSED THE NOTIFICATION!");
+    });
+  }
+}
+
+document.querySelector("#btn").addEventListener("click", showNotification);
